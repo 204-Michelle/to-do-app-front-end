525b1423c02156aa9f0d9501890153a55a36f01a
Michelle Cadavid <cadavidcarmona1@gmail.com>
2023-03-17 01:30:07 -0500

    vite config changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { makeStyles } from '@mui/material'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [
+        '@mui/material'
+      ]
+    }
+  }
 })
