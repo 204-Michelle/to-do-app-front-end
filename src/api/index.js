911657f0936bcd2d41e8b1a5ac6a7180e5bea03f
Michelle Cadavid <cadavidcarmona1@gmail.com>
2023-03-17 00:40:09 -0500

    redirect and api url

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,6 @@
-export const url = "http://localhost:5000/api"
+//export const url = "http://localhost:5000/api"
+export const url = "https://to-do-app-back-end-production.up.railway.app/api"
+
 
 export const setHeaders = () => {
     const header = {
